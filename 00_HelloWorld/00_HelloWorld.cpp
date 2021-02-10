873e5fe2fc981348310278bb531ea9c89995ed63
Paiva_Desktop <lucaspaiva.luc@gmail.com>
2021-02-10 16:31:33 -0300

    primeiros arquivos

---
+++ 00_HelloWorld/00_HelloWorld.cpp
@@ -0,0 +1,10 @@
+// Lucas Paiva da Silva - 20100417
+// INE5201 - Atividade 01 -> Primeiro Programa
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nois que voa bruxao" << endl;
+}
---
+++ Misk/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float lado;
+    cout<<"Entre o lado do quadrado: ";
+    cin >> lado;
+
+
+    cout<< "O perimetro é de " << (lado*4) << " u.a." << endl;
+}
